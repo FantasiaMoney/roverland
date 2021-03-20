f95ec89f053640e81341cc819fcfa2ac7f86edd4
Gary Bernstein <gary@cotrader.com>
2021-03-20 10:55:31 -0700

    GnPu tracking and rm screen_view from GA

--- js/google-analytics-config.js
+++ js/google-analytics-config.js
@@ -12,9 +12,9 @@ $(document).ready(function ($) {
         // event.preventDefault();
         var getCickedButtonValue = $(this).attr('value');
         // console.log("Google analytics User Clicked on ", getCickedButtonValue);
-        gtag('event', 'screen_view', {
-            'screen_name': getCickedButtonValue
-        });
+        // gtag('event', 'screen_view', {
+        //     'screen_name': getCickedButtonValue
+        // });
         gtag('event', "Clkd" + getCickedButtonValue, {
             'source': getCickedButtonValue
         });
@@ -26,9 +26,9 @@ $(document).ready(function ($) {
         // event.preventDefault();
         var getCickedButtonValue = $(this).attr('value');
         // console.log("Google analytics User Clicked on ", getCickedButtonValue);
-        gtag('event', 'screen_view', {
-            'screen_name': getCickedButtonValue
-        });
+        // gtag('event', 'screen_view', {
+        //     'screen_name': getCickedButtonValue
+        // });
         gtag('event', "Clkd" + getCickedButtonValue, {
             'source': getCickedButtonValue
         });
